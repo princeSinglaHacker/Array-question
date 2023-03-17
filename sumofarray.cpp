159c9caa0e0f81c0744c7e88ede2f2d9f8c0ae91
Prince <singlaprince349@gmail.com>
2023-03-17 15:36:15 -0300

    secondcommit

--- sumofarray.cpp
+++ sumofarray.cpp
@@ -4,9 +4,11 @@ main()
 {
 	int arr[10];
 	printf("Enter ten numbers for sum: \n");
-	int sum=0;
 	for(int i=0;i<10;i++){
 		scanf("%d",&arr[i]);
+	}
+	int sum=0;
+	for(int i=0;i<=9;i++){
 		sum=sum+arr[i];
 	}
 	printf("Ans is: %d\n",sum);
