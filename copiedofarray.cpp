77354184c909513182b155aae7c59d6dcff2af68
Prince <singlaprince349@gmail.com>
2023-03-20 15:14:58 -0300

    before CONFLICT

--- copiedofarray.cpp
+++ copiedofarray.cpp
@@ -2,15 +2,20 @@
 #include<conio.h>
 main()
 {
-	int arr[5]={63,41,87,96,52};
-	int arr1[5];
+	int size;
+	printf("Enter size of array: \n");
+	scanf("%d",&size);
+	int arr[size];
+	int arr1[size];
 	int k=0;
-	for(int i=0;i<5;i++){
+	printf("Enter %d numbers: \n",size);
+	for(int i=0;i<size;i++){
+		scanf("%d",&arr[i]);
 		arr1[k]=arr[i];
 		k++;
 	}
 	printf("After copied we will print another array: \n");
-	for(int j=0;j<5;j++){
+	for(int j=0;j<size;j++){
 		printf("%d\n",arr1[j]);
 	}
 }
